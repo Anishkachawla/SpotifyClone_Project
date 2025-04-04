50d43fa5eab4a5802207f7dad739bd937fc02735
Anishkachawla <davanishka10i10@gmail.com>
2025-04-04 12:18:11 +0530

    Day 7 Part 1

---
+++ script.js
@@ -0,0 +1,10 @@
+async function main(){
+    let a = await fetch("http://127.0.0.1:5500/SpotifyClone_Project/songs/")
+    let response = await a.text()
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let tds =div.getElementsByTagName("td")
+    console.log(tds)
+}
+main()
